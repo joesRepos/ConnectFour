c949049446d3bc89d1d9c5840e61e85498a2e56c
joesRepos <joemitchell1717@gmail.com>
2026-03-09 18:27:20 +0000

    Initilaise the board.

--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void InitialiseBoard(char[,] board)
+{
+    for (int r = 0; r < 6; r++)
+    {
+        for (int c = 0; c < 7; c++)
+        {
+            board[r, c] = '.';
+        }
+    }
+}
+
+char[,] board = new char[6,7];
+InitialiseBoard(board);
